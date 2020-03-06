1e89bae4da2d4a189ce30a8b98625b8cf5d7576c
Ramin Gharib <ramingharib@gmail.com>
2020-03-06 17:01:48 +0100

    clean imports

--- src/main/java/org/bakdata/kafka/challenge/s3/S3Handler.java
+++ src/main/java/org/bakdata/kafka/challenge/s3/S3Handler.java
@@ -31,7 +31,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import org.apache.kafka.clients.consumer.StickyAssignor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
